97ad4cafe615f703a2564f72aee6c7b464f7a50a
Артур Кустяев <95183545+ArturKustyaev@users.noreply.github.com>
2022-08-07 22:25:53 +0400

    delete UI folder

--- src/UI/Container/Container.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactNode } from 'react'
-import classes from './Container.module.scss'
-
-interface Props {
-	children: ReactNode
-}
-export const Container: FC<Props> = ({ children }): JSX.Element => {
-	return <div className={classes.container}>{children}</div>
-}
